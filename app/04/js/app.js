b5bc2d3ce58d6aca6c20b0a64385ba257bc2593d
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-01 13:16:12 -0300

    Directiva que se encarga de la verficacion del nombre de usuario

---
+++ app/04/js/app.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+angular.module('validacionApp', [])
+        .directive('ensureUnique', function($http){
+            return {
+              require: 'ngModel',
+              link: function(scope, ele, attrs, c){
+                  scope.$watch(attrs.ngModel, function(n){
+                     if(!n)return;
+                     $http({
+                         method:'POST',
+                         url:'/api/check/'+attrs.ensureUnique,
+                         data:{'field':attrs.ensureUnique}
+                     }).success(function(data){
+                        c.$setValidity('unique', data.isUnique);
+                     }).error(function(data){
+                        c.$setValidity('unique', false); 
+                     });
+                  });
+              }
+            };
+});
+
