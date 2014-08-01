523493b54f8a31794ce4945280506580755e7c3d
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-08-01 16:48:03 -0300

    Elementos, Atributos, Clases y Comentarios como directivas

--- app/05/js/app.js
+++ app/05/js/app.js
@@ -6,7 +6,7 @@
 angular.module('myApp', [])
     .directive('myDirective', function(){
         return {
-          restrict: 'E',
+          restrict: 'EACM',//E es un elemento, A atributo, C clase, M comentario
           replace: true,//eliminar <my-directive> del DOM
           template: '<a href="http://google.com"> Go to Google</a>'
         }
